f22a4e6ad0b3afc842fbf0dce0ace33e67555992
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-27 16:29:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
+}
 
-  this.prototype.veto = function(){
+  BoardMember.prototype.veto = function(){
     return 'No, I must disagree'
   }
-  this.approve = function(){
+  BoardMember.prototype.approve = function(){
     return 'You can do that!'
   }
   this.doCharity = function(){
